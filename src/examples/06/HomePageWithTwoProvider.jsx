51f1476c78ecf98d62c730e938db76f27756bb4a
SungJaeJO <xc11161@gmail.com>
2021-03-05 17:38:10 +0900

    Type Error occur!!! : loading of context undefined

---
+++ src/examples/06/HomePageWithTwoProvider.jsx
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react';
+import { 
+    ButtonWithDefaultLoadingContext, 
+    ButtonWithLoading2Context
+} from './ButtonWithLoadingContextAndKey';
+import LoadingProviderWithKey, { DEFAULT_KEY } from './LoadingProviderWithKey';
+
+const LoadingProvider1 = LoadingProviderWithKey();
+const LoadingProvider2 = LoadingProviderWithKey('loading2');
+
+function TableComponent() {
+    return (
+        <table>
+            <ButtonWithDefaultLoadingContext>컨텍스트1</ButtonWithDefaultLoadingContext>
+            <ButtonWithLoading2Context>컨텍스트2</ButtonWithLoading2Context>
+        </table>
+    );
+}
+
+export default class HomePageComponentWithTwoProvider extends PureComponent {
+    render() {
+        return (
+            <LoadingProvider1>
+                <LoadingProvider2>
+                    <TableComponent />
+                </LoadingProvider2>
+            </LoadingProvider1>
+        )
+    }
+}
--- src/stories/ContextStory.jsx
+++ src/stories/ContextStory.jsx
@@ -3,7 +3,9 @@ import { storiesOf } from '@storybook/react';
 
 import HomePageComponent from '../examples/06/HomePageComponent';
 import HomePageWithProvider from '../examples/06/HomePageWithProvider';
+import HomePageComponentWithTwoProvider from '../examples/06/HomePageWithTwoProvider';
 
 storiesOf('HomePageComponent', module)
     .addWithJSX('컨텍스트 예제', () => <HomePageComponent />)
-    .addWithJSX('Provider 예제', () => <HomePageWithProvider />);
+    .addWithJSX('Provider 예제', () => <HomePageWithProvider />)
+    .addWithJSX('이중 Provider 예제', () => <HomePageComponentWithTwoProvider />);
